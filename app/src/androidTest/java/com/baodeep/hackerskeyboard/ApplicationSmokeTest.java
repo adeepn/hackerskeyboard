cb967ffdd3891b4cd230408e71c0822a6bab7877
Viacheslav Bocharov <v@baodeep.com>
2026-08-15 12:13:35 +0300

    Verify the single restored settings fragment (#61)

--- app/src/androidTest/java/com/baodeep/hackerskeyboard/ApplicationSmokeTest.java
+++ app/src/androidTest/java/com/baodeep/hackerskeyboard/ApplicationSmokeTest.java
@@ -138,8 +138,7 @@ public class ApplicationSmokeTest {
 
     private static ListPreference assertActionsPreferences(PrefScreenActions activity) {
         assertEquals(1, activity.getSupportFragmentManager().getFragments().size());
-        Fragment fragment = activity.getSupportFragmentManager()
-                .findFragmentById(android.R.id.content);
+        Fragment fragment = activity.getSupportFragmentManager().getFragments().get(0);
         assertNotNull("Actions preference fragment is missing", fragment);
         assertTrue(fragment instanceof PrefScreenActions.ActionsPreferenceFragment);
 
